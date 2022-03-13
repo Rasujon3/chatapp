c3da41fd5e076f53885c4e2874449954ed4d2f4c
Rasujon3 <rasujon3@gmail.com>
2022-03-13 16:35:22 +0600

    react-chat-engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <ChatEngine
+        height="100vh"
+        // userName="sujon"
+        userName="john"
+        userSecret="qwerty"
+        // userSecret="sujon"
+        projectID="3342a8fa-120f-46d0-953f-942e20ffff81"
+      />
     </div>
   );
 }
