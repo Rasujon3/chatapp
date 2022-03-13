6ec16442022aa552c93d881d69588ec5614f5aa6
Rasujon3 <rasujon3@gmail.com>
2022-03-13 17:45:01 +0600

    their message components setup

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,38 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstmessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+
   return (
-    <div>
-      <h1>TheirMessage</h1>
+    <div className="message-row">
+      {isFirstmessageByUser && (
+        <div
+          className="message-avater"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
+      )}
+
+      {message.attachments && message.attachments.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstmessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            marginLeft: isFirstmessageByUser ? "4px" : "48px",
+            color: "white",
+            backgroundColor: "#CABCDC",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
     </div>
   );
 };
